4c4255ccb136b53f2e84bfbfeba34062e31aa64e
Hugo <hugo_valentin.etu@univ-lemans.fr>
2021-01-25 14:27:49 +0100

    Connexion+arbitrage

---
+++ Appli_mobile/app/src/main/java/com/example/iotia/arbitrage.java
@@ -0,0 +1,14 @@
+package com.example.iotia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class arbitrage extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.arbitrage);
+    }
+}
